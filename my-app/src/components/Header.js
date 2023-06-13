bc657c957e575952495f9115db7b747280297ec3
Isaac Stofko <isaacstofko@gmail.com>
2023-06-13 14:59:06 -0400

    styling

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -5,15 +5,26 @@ const styles = {
     headingStyle: {
       fontSize: '100px',
       display: 'flex',
-      justifyContent: 'flex-end'
+      justifyContent: 'flex-start'
     },
+    imgStyle: {
+        width: '30%',
+        height: 'auto',
+        padding: '10px',
+        /* display: 'flex', */
+        float: 'right'
+    }
   };
+
 
 function Header() {
     return (
       <header className="header">
-        <h1 style={styles.headingStyle}>Isaac Stofko</h1>
-         <img src="./assets/prof_pic.jpeg" alt="a side profile of me at the Museum of Modern Art in New York City"></img> 
+        <div className="header-body">
+        <h1 style={styles.headingStyle}>Isaac Stofko
+         <img src="https://i.imgur.com/0KsY1OR.jpg" alt="a side profile of me at the Museum of Modern Art in New York City" style={styles.imgStyle}></img> 
+         </h1>
+         </div>
         <h2>Full Stack Developer</h2>
       </header>
     );
