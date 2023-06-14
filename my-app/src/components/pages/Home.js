aa72624e9be696f57bb8a8f8a722c9bbe30c45a5
Isaac Stofko <isaacstofko@gmail.com>
2023-06-14 17:04:17 -0400

    added header image

--- my-app/src/components/pages/Home.js
+++ my-app/src/components/pages/Home.js
@@ -1,40 +1,23 @@
 import React from 'react';
 import '../../App.css'
 
-const styles = {
-  imgStyle: {
-      width: '30%',
-      height: 'auto',
-      margin: '20px',
-
-  },
-  headingStyle: {
-    fontSize: '100px',
-    display: 'flex',
-    justifyContent: 'flex-end',
-    float: 'right'
-  },
-  subHeadStyle: {
-      
-      height: 'auto',
+const style = {
+  containerStyle: {
+    backgroundImage: 'url("https://i.imgur.com/UB3zdiP.png")',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    height: '100vh',
+    width: '100vw',
   }
+}
 
-};
 
 function Home() {
   return (
   
-  <header className="header">
-      <div className="header-body">
-    
-     
-    <img src="https://i.imgur.com/r9fdyL3.jpg" alt="Rene Magritte's painting of two figures standing in the sky imposed over a window" style={styles.imgStyle}/> 
-    
-       
+      <div className="home-body" style={style.containerStyle}>
       </div>
-    
-      
-      </header>
   );
 }
 
