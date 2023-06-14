cbce7237004600983ddbf9da04441aa44e7c10b5
Isaac Stofko <isaacstofko@gmail.com>
2023-06-14 16:57:40 -0400

    header -> home

--- my-app/src/components/pages/Home.js
+++ my-app/src/components/pages/Home.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import '../../App.css'
 
-export default function Home() {
+const styles = {
+  imgStyle: {
+      width: '30%',
+      height: 'auto',
+      margin: '20px',
+
+  },
+  headingStyle: {
+    fontSize: '100px',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    float: 'right'
+  },
+  subHeadStyle: {
+      
+      height: 'auto',
+  }
+
+};
+
+function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
-    </div>
+  
+  <header className="header">
+      <div className="header-body">
+    
+     
+    <img src="https://i.imgur.com/r9fdyL3.jpg" alt="Rene Magritte's painting of two figures standing in the sky imposed over a window" style={styles.imgStyle}/> 
+    
+       
+      </div>
+    
+      
+      </header>
   );
 }
+
+export default Home;
