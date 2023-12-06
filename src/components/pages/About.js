f35e355cef655896b867a2d78b8d505499b46e17
Isaac Stofko <istofko@contractornation.com>
2023-12-06 14:19:55 -0500

    p

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -24,9 +24,13 @@ function About() {
         <p>
           I am a new web developer equipped with a robust educational and
           professional foundation in English Literature, Creative Writing, Art History, 
-          Hospitality, and Team Management. Alongside these skills, I possess
+          Hospitality, and Team Management.
+          </p> 
+          <p>Alongside these skills, I possess
           comprehensive knowledge of JavaScript, React, Node.js, MongoDB, REST
-          APIs, CSS, HTML, and SQL among other languages and technologies. I
+          APIs, CSS, HTML, and SQL among other languages and technologies. 
+          </p>
+          <p>I
           eagerly anticipate integrating my imaginative and analytical mindset
           into the technology industry, as I embark on an interdisciplinary
           approach to problem-solving.
