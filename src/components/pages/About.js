66223b0693193b2e77419827e9d64b97370d819c
Isaac Stofko <isaacstofko@gmail.com>
2024-03-07 15:35:37 -0500

    resolve conflicts

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -27,12 +27,11 @@ function About() {
           I am a full stack web developer equipped with a robust educational and
           professional foundation in English Literature, Creative Writing, Art History, 
           Hospitality, and Team Management.
-          </p> 
-          <p>Alongside these skills, I possess
+          
+          Alongside these skills, I possess
           comprehensive knowledge of JavaScript, React, Node.js, MongoDB, REST
           APIs, CSS, HTML, and SQL among other languages and technologies. 
-          </p>
-          <p>I
+          I
           eagerly anticipate integrating my imaginative and analytical mindset
           into the technology industry, as I embark on an interdisciplinary
           approach to problem-solving.
