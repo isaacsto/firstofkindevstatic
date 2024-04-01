32c740416d08b96d24d521a1dc0be3e297ad7fd3
Isaac Stofko <isaacstofko@gmail.com>
2024-04-01 18:29:35 -0400

    css

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -17,7 +17,7 @@ function About() {
           <img
             src="https://i.imgur.com/gylZ6Zs.jpg"
             alt="" 
-            className="my-pic" width="200"
+            className="my-pic" width="200" 
           />
       </div>
     
@@ -26,12 +26,15 @@ function About() {
 
         <p>
           I am a full stack web developer equipped with a robust educational and
-          professional foundation in English Literature, Creative Writing, Art History, Hospitality, and Team Management. I am passionate about creating beautiful and user friendly UIs and mobile-first frontend development. I am equally passionate about utilizing backend frameworks and libraries to create dynamic, functional, and streamlined backends in order to connect  
-          
+          professional foundation in English Literature, Creative Writing, Art History, Hospitality, and Team Management. I am passionate about creating beautiful and user friendly UIs and mobile-first frontend development. I am equally passionate about utilizing backend frameworks and libraries to create dynamic, scalable, and streamlined backends, seeking to balance style with utility. 
+          </p>
+          <p>
           I possess comprehensive knowledge of JavaScript, React, Node.js, MongoDB, REST APIs, CSS, HTML, and SQL among other languages and technologies. 
-
-          My special interests include AI and music software including DAWs and special affect plugins. 
-
+          </p>
+          <p>
+          I am especially interested in exploring AI and music software including DAWs and special affect plugins as I move through my career. 
+          </p>
+          <p>
           I eagerly anticipate integrating my imaginative and analytical mindset
           into the technology industry, as I embark on an interdisciplinary
           approach to problem-solving, creating, and innovating. 
