205f91a43ee6d516abeb28ca0b6090c1bc712b66
Isaac Stofko <isaacstofko@gmail.com>
2025-01-07 15:37:54 -0500

    typo

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -14,7 +14,7 @@ function About() {
   professional foundation in English Literature, Creative Writing, Art History, Hospitality, and Team Management. I am passionate about creating beautiful and user friendly UIs and mobile-first frontend development. I am equally passionate about utilizing backend frameworks and libraries to create dynamic, scalable, and streamlined backends, seeking to balance style with utility. 
   </p>
   <p>
-  I possess comprehensive knowledge of JavaScript, React, Node.js, MongoDB, REST APIs, CSS, HTML, and SQL among other languages and technologies. As I progress in my career, I am particularly interested on delving deeper into the realms of artificial intelligence (AI) in its uses for marketing and advertising, as well as its potential for improvimg music software, which encompasses digital audio workstations (DAWs) and plugins. To pursue this interest, I have embarked on a journey of gaining Machine Learning certifications through Google & Coursera recognizing the potential in this field.
+  I possess comprehensive knowledge of JavaScript, React, Node.js, MongoDB, REST APIs, CSS, HTML, and SQL among other languages and technologies. As I progress in my career, I am particularly interested on delving deeper into the realms of artificial intelligence (AI) in its uses for marketing and advertising, as well as its potential for improving music software, which encompasses digital audio workstations (DAWs) and plugins. To pursue this interest, I have embarked on a journey of gaining Machine Learning certifications through Google & Coursera recognizing the potential in this field.
   </p>
   <p>You can contact me at isaacstofko@gmail.com</p>
 
