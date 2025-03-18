263422fa38e38f274f4fd36f18e71fdb4ac35950
Isaac Stofko <isaacstofko@gmail.com>
2025-03-18 15:12:25 -0400

    css

--- src/components/pages/Header.js
+++ src/components/pages/Header.js
@@ -5,8 +5,7 @@ function Header({ currentPage, handlePageChange }) {
   return (
     <div className="header-body" id="header">
   
-        <h1 className="name">First of Kind Development and Design</h1>
-        <h2>Isaac Stofko</h2>
+        <h1 className="name">Isaac Stofko</h1>
         <h2 className="title">Full Stack Web Developer</h2>
 
       <div className="nav-wrap">
