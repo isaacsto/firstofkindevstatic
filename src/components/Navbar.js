c80efb53eef485117e539a322fed08faad2172c5
Isaac Stofko <isaacstofko@gmail.com>
2025-03-18 15:11:13 -0400

    nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,6 @@ function Navbar({ currentPage, handlePageChange }) {
 <li className="nav-item"> <a href="#work">Work</a></li>
 <li className="nav-item"> <a href="#resume">Resume</a></li>
 <li className="nav-item"> <a href="#about">About</a></li>
-<li className="nav-item"> <a href="#freelance">Consultation</a></li>
     </ul>
   );
 }
